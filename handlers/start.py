5dd606e65cd11228b2138653bd792709e65e2a07
ibras0696 <ibras0696>
2025-07-16 19:12:35 +0300

    возрат реферальной системы

--- handlers/start.py
+++ handlers/start.py
@@ -13,6 +13,28 @@ from database import CrudeUser
 
 router = Router()
 
+# Обработка старта через реферальную ссылку
+@router.message(CommandStart(deep_link=True))
+async def start_cmd(message: Message, state: FSMContext, command: CommandStart):
+    # Чистка состояний
+    await state.clear()
+
+    # Проверка передачи реферального кода
+    ref_id = command.args
+    try:
+        if ref_id:
+            r_d = int(ref_id)
+            # Добавляем в Базу
+            await CrudeUser().add_user(telegram_id=message.from_user.id,
+                                       user_name=message.from_user.username,
+                                       referral_id=r_d)
+            await message.answer(message_texts.start_text, reply_markup=start_kb)
+    # Если ведены доп параметры
+    except ValueError:
+        pass
+    except Exception as ex:
+        print(f'Ошибка: {ex}')
+
 # Обработка обычного старта
 @router.message(CommandStart())
 async def start_cmd(message: Message, state: FSMContext):
