ddf98c033960c9123afffbf0bfe117470c68f777
ibras0696 <ibras0696@gmail.com>
2025-07-16 18:31:12 +0300

    добавление yml файла

--- utils/shedluer_func.py
+++ utils/shedluer_func.py
@@ -5,7 +5,7 @@ from apscheduler.triggers.cron import CronTrigger
 
 from config import TOKEN_BOT
 from database import CrudeSubscriptions, Subscriptions
-from utils.message_texts import push_subs_text
+from utils.message_texts import push_subs_text, end_push_sub_text
 from utils.admin_kick_func import kick_user_if_not_admin
 
 # Экземпляр бота для работы
@@ -14,6 +14,18 @@ bot_tg = Bot(token=TOKEN_BOT)
 
 async def users_push_and_kick_group(bot: Bot = bot_tg):
     sbu_con = CrudeSubscriptions()
+    subs_check = await sbu_con.get_all_users_subscriptions()
+    if subs_check:
+        for sub in subs_check:
+            if sub.day_count == 1:
+                try:
+                    # айди групп
+                    grp_id = Subscriptions.get_group_ids().get(sub.plan)
+                    await bot.send_message(sub.telegram_id, await end_push_sub_text(plan=grp_id))
+                    if grp_id:
+                        await kick_user_if_not_admin(bot, sub.telegram_id, grp_id)
+                except:
+                    pass
     # Полуычение данных подписок
     subs = await sbu_con.all_reduce_subscriptions()
     if subs:
@@ -24,11 +36,10 @@ async def users_push_and_kick_group(bot: Bot = bot_tg):
                 try:
                     # айди групп
                     grp_id = Subscriptions.get_group_ids().get(sub.plan)
-                    print('\n\n\n', grp_id, '\n\n\n')
                     if grp_id:
                         await kick_user_if_not_admin(bot, sub.telegram_id, grp_id)
-                except Exception as ex:
-                    print(f'Ошибка при удаление с группы: {ex}')
+                except:
+                    pass
 
 
 def setup_scheduler(hour: int = 0, minute: int = 0) -> AsyncIOScheduler:
