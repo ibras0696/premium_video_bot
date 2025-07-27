4e87ac8b81fa152278057e01408bb40cb5538417
ibras0696 <ibras0696>
2025-07-27 22:43:07 +0300

    изменения цены

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -2,6 +2,7 @@ from aiogram import Router
 
 from .admin_static import router as admin_static_router
 from .admin_tg_groups import router as admin_tg_groups_router
+from .admin_pay_handler_test import router as admin_pay_handler_test_router
 
 router = Router()
 
@@ -9,4 +10,5 @@ router = Router()
 router.include_routers(
     admin_static_router, # Static admin commands
     admin_tg_groups_router, # Admin TG groups management
+    admin_pay_handler_test_router, # Admin payment handler test
 )
